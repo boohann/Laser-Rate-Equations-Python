868462950aa02a29b71138d3feed3ca26a665107
Niall Boohan <boohann@tcd.ie>
2026-05-07 09:04:23 +0100

    Tidy laser rate equations simulation code
    Refactor laser simulation code to improve structure and readability. Introduce LASER_PARAMS dictionary and SimConfig class for better parameter management.

--- RateEqns.py
+++ RateEqns.py
@@ -15,49 +15,57 @@
 from scipy.integrate import ode
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import cons
 
 ### Select calculation ###
 CALC = 0        # 0 is dynamic, 1 is steady-state LI
 
 ### Simulation Outputs ###
-N     = []      # y[0] Carrier concentration
-S     = []      # y[1] Photon concentration
-T     = []      # Time array output
-N_end = []      # Take the final N value for steady-state behaviour
-S_end = []      # Take the final S value for steady-state behaviour
+#N     = []      # y[0] Carrier concentration
+#S     = []      # y[1] Photon concentration
+#T     = []      # Time array output
+#N_end = []      # Take the final N value for steady-state behaviour
+#S_end = []      # Take the final S value for steady-state behaviour
 
 ### Simulation input parameters ###
-IA      = 20                                      # Pumping current (mA)
-I       = IA/1e3                                  # Pumping current (A)
+                                
 iIA     = np.linspace(0, 50, 100)                 # Generate multiple I for LI curve (mA)
 iI      = [x/1e3 for x in iIA]                    # Multiple I (A)
-α       = 5                                       # Cavity lasing mode loss (cm^-1)
-n       = 3.2                                     # Cavity refractive index
-q       = 1.6e-19                                 # Electron charge (C)
-L       = 400                                     # Cavity length (um)
-w       = 2                                       # Cavity width (um)
-h       = 100                                     # Height of active region (nm)
-V       = L*w*h*(1e-15)                           # Device volume (cm^3), cm^3 conversion factor
-r_l     = 0.5                                     # Left facet reflectivity
-r_r     = 0.5                                     # Right facet reflectivity
-tn      = 1.0e-9                                  # Carrier relaxation time in seconds (s)
-g0      = 1.5e-5                                  # Gain slope constant (cm^3s^-1)
-Nth     = 1e18                                    # Threshold carrier density (cm^-3)
-EPS     = 1.5e-17                                 # Gain compression factor (cm^3)
-Gamma   = 0.15                                    # Confinement factor
-Beta    = 1.0e-4                                  # Spontaneous Emission Factor
-h       = 6.62607004e-34                          # Plank's constant (Js)
-c       = 2.99792458e8                            # SOL (ms^-1)
-WL      = 1300                                    # WL (nm)
-f       = c/(WL/1e9)                              # Frequency (Hz)
-tp      = 1/((c/(L*1e-6))*np.log(1/(r_l*r_r)))    # Photon round-trip time in cavity (s)
-tα      = 1/(c*α*100)                             # Photon lifetime material loss (s)
 
 
+LASER_PARAMS = {
+    'I': 20/1e3,            # Pumping current (A)
+    'α': 5,                 # Cavity lasing mode loss (cm^-1)
+    'n': 3.2,               # Cavity refractive index
+    'L': 400,                # Cavity length
+    'w': 2,                   # Cavity width
+    'h_active': 100,            # Height active region (nm)
+    'V': L*w*h*(1e-15),        # Volume active region  (cm^3)    
+    'r_l': 0.5,                # Left amplitude reflectivity
+    'r_r': 0.5,             # Left amplitude reflectivity
+    'β':1e-4,             # Spontaneous Emission Factor
+    'Γ':0.15,            # Quantum well confinement factor
+    'τ_n':1.0e-9                                  # Carrier relaxation time in seconds (s)
+    'τ_p':1/((cons.c/(L*1e-6))*np.log(1/(r_l*r_r)))    # Photon round-trip time in cavity (s)
+    'τ_α':1/(cons.c*α*100)                             # Photon lifetime material loss (s)
+    'g_0':1.5e-5                                  # Gain slope constant (cm^3s^-1)
+    'N_tr':1e17                                    # Transparency carrier density (cm^-3)
+    'EPS':1.5e-17                                 # Gain compression factor (cm^3)
+    'WL':1300                                    # WL (nm)
+    'f':cons.c/(WL/1e9)                              # Frequency (Hz)   
+}
+
+# Config hard-coded values ###
+class SimConfig:
+    T_START = 0
+    T_END = 5e-9
+    DT = 1e-13
+    N_INITIAL = 1e16
+    S_INITIAL = 0
 
 def call_solv(x):
 
-    ### Ensures global values of S, N and T are updated from this function ###
+    ### Ensures global values of S, N, and T are updated from this function ###
     global S
     global N
     global T
@@ -66,26 +74,26 @@ def call_solv(x):
     def laser_rates(t, y, p):
         
         dy = np.zeros([2])
-        dy[0] = (x/(q* V)) - (y[0]/tn) -  g0*(y[0] - Nth)*(y[1]/(1 + EPS* y[1]))
-        dy[1] = Gamma* g0* (y[0] - Nth)*(y[1]/(1 + EPS* y[1])) - y[1]/(tp+tα) + (Gamma* Beta* y[0]) / tn
+        dy[0] = (x/(q* V)) - (y[0]/τ_n) -  g0*(y[0] - N_tr)*(y[1]/(1 + EPS* y[1]))
+        dy[1] = Γ* g0* (y[0] - N_tr)*(y[1]/(1 + EPS* y[1])) - y[1]/(tp+tα) + (Γ* β* y[0]) / tn
         
         return dy
         
 
     ### Time, initial conditions & add paramters ###  
-    t0 = 0; tEnd = 5e-9; dt = 1e-13                     # Time constraints
+    #t0 = 0; tEnd = 5e-9; dt = 1e-13                     # Time constraints
     y0 = [1e16, 0]                                      # Initial conditions [N, S]
     Y=[]; T=[]                                          # Create empty lists
-    p = [I, q, V, tn, g0, Nth, EPS, Gamma, tp, Beta]    # Parameters for odes
+    #p = [I, q, V, tn, g0, Nth, EPS, Γ, tp, Beta]    # Parameters for odes
 
 
     ### Setup integrator with desired parameters ###
     # Runge-Kutta must be used as a solver, minimum 4th order
     r = ode(laser_rates).set_integrator('dopri5', nsteps = 1e4)
-    r.set_f_params(p).set_initial_value(y0, t0)
+    r.set_f_params(LASER_PARAMS).set_initial_value([N_INITIAL, S_INITIAL], T_START = 0)
 
     
-    ### Simualtion check ###
+    ### Simulation check ###
     while r.successful() and r.t+dt < tEnd:
         r.integrate(r.t + dt)
         Y.append(r.y)        # Makes a list of 1d arrays
@@ -98,14 +106,11 @@ def call_solv(x):
     S = Y[:, 1] 
 
     ### Take final value for steady-state LI ###
-    S_end.append(S[-1:])
-    N_end.append(N[-1:])
-
-    return;
+    return T, N, S, N[-1:], S[-1:]
 
 
 ### Dynamic plotting ###
-def plot_dynam():
+def plot_dynam(T, N, S):
 
     f, axarr = plt.subplots(2, sharex=True) # Two subplots, the axes array is 1-d
     axarr[0].plot(T, N, 'g-')
@@ -115,6 +120,7 @@ def plot_dynam():
     axarr[1].set_ylabel("Photon concentration ($cm^{-3}$)")
     axarr[1].set_xlabel("Time (s)")
     plt.show()
+    plt.close()
 
     return;
 
@@ -123,7 +129,7 @@ def plot_dynam():
 def plot_SS():
     
     ### Post-solver calculations
-    P = [h*f*((i*V)/tp)*1e3 for i in S_end]        # Power output (mW)
+    P = [cons.h*f*((i*V)/tp)*1e3 for i in S_end]        # Power output (mW)
     QE = [i/j for i,j in zip(P, iIA)]              # Convert for quantum efficiency
 
     ### Plotting two parameters on one plot ###
@@ -136,18 +142,21 @@ def plot_SS():
     ax2.set_ylabel('Quantum efficiency', color='b')
     plt.title("Steady-state solution")
     plt.show()
+    plt.close()
 
     return;
 
 
 ### Dynamic mode ###
 if(CALC == 0):
-    call_solv(I)
-    plot_dynam()
+    T, N, S, _, _ = call_solv(I)
+    plot_dynam(T, N, S)
 
 
 ### Steady-state mode ###
 if(CALC == 1):
+    S_final = []
     for i in iI:
-        call_solv(i)
-    plot_SS()
+        _, _, _, _, S_hld = call_solv(i)
+        S_final.append(S_hld)
+    plot_SS(S_hld)
